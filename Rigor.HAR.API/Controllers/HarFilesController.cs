3b22efda1f59efd6da8eb77c02032e03cef924db
Marc Robards <marc@robards.org>
2018-02-09 09:22:05 -0500

    Implemented validation

--- Rigor.HAR.API/Controllers/HarFilesController.cs
+++ Rigor.HAR.API/Controllers/HarFilesController.cs
@@ -1,12 +1,16 @@
 ﻿namespace Rigor.HAR.API.Controllers
 {
+    using HarSharp;
     using Microsoft.AspNetCore.Mvc;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
+    using Newtonsoft.Json.Schema;
     using Rigor.HAR.API.Models;
     using Rigor.HAR.API.Services;
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
 
     [Produces("application/json")]
@@ -35,62 +39,104 @@
         }
 
         [HttpPost]
-        public async Task Post([FromBody]JObject harFileData)
+        public async Task<IActionResult> Post([FromBody]JObject harFileData)
         {
             if (harFileData != null)
             {
-                var harFile = new HarFile();
+                var harData = HarConvert.Deserialize(harFileData.ToString());
 
-                if (harFileData["log"] != null)
+                var valid = this.ValidateHarFile(harData);
+
+                if (valid)
                 {
-                    var log = harFileData["log"];
-                    if (log["pages"] != null)
-                    {
-                        var pages = log["pages"];
-                        var firstPage = pages[0];
+                    var firstPage = harData.Log.Pages.First();
 
-                        harFile.URL = (string)firstPage["title"];
-                        harFile.StartedDateTime = DateTime.Parse(firstPage["startedDateTime"].ToString());
-                        harFile.JSONString = JsonConvert.SerializeObject(harFileData);
+                    var harFile = new HarFile();
+                    harFile.URL = firstPage.Title;
+                    harFile.StartedDateTime = firstPage.StartedDateTime;
+                    harFile.JSONString = JsonConvert.SerializeObject(harFileData);
 
-                        await this._harFilesService.SaveAsync(harFile);
-                    }
+                    await this._harFilesService.SaveAsync(harFile);
+                }
+                else
+                {
+                    return BadRequest("The HAR file data is invalid.");
                 }
             }
+
+            return Ok();
         }
 
         [HttpPut("{id}")]
-        public async Task Put(long id, [FromBody]JObject harFileData)
+        public async Task<IActionResult> Put(long id, [FromBody]Har harData)
         {
-            if (harFileData != null)
+            if (!ModelState.IsValid)
             {
-                var existingHarFile = await this._harFilesService.GetByIdAsync(id);
-
-                if (existingHarFile != null)
-                {
-                    if (harFileData["log"] != null)
-                    {
-                        var log = harFileData["log"];
-                        if (log["pages"] != null)
-                        {
-                            var pages = log["pages"];
-                            var firstPage = pages[0];
-
-                            existingHarFile.URL = (string)firstPage["title"];
-                            existingHarFile.StartedDateTime = DateTime.Parse(firstPage["startedDateTime"].ToString());
-                            existingHarFile.JSONString = JsonConvert.SerializeObject(harFileData);
-
-                            await this._harFilesService.UpdateAsync(existingHarFile);
-                        }
-                    }
-                }
+                return BadRequest(ModelState);
             }
+
+            var existingHarFile = await this._harFilesService.GetByIdAsync(id);
+
+            if (existingHarFile != null)
+            {
+                var firstPage = harData.Log.Pages.First();
+
+                var harFile = new HarFile();
+                existingHarFile.URL = firstPage.Title;
+                existingHarFile.StartedDateTime = firstPage.StartedDateTime;
+                existingHarFile.JSONString = JsonConvert.SerializeObject(harData);
+
+                await this._harFilesService.UpdateAsync(existingHarFile);
+            }
+
+            return Ok();
         }
 
         [HttpDelete("{id}")]
-        public async Task Delete(long id)
+        public async Task<IActionResult> Delete(long id)
         {
             await this._harFilesService.DeleteAsync(id);
+
+            return Ok();
+        }
+
+        private bool ValidateHarFile(Har harData)
+        {
+            try
+            {
+                var page = harData.Log.Pages.FirstOrDefault();
+
+                if (page == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    // validate title exists
+                    if (string.IsNullOrEmpty(page.Title))
+                    {
+                        return false;
+                    }
+
+                    // validate startedDateTime exists
+                    if (page.StartedDateTime == DateTime.MinValue)
+                    {
+                        return false;
+                    }
+                }
+
+                // validate entries exist
+                if (harData.Log.Entries.Count == 0)
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
